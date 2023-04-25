0ae1ac3c23ae4ad24c92901d8a2d3ba4fdfa4663
Hiroshi Suzuki <hiroshi.btree@outlook.jp>
2023-04-25 21:28:12 +0900

    ✨ useState 　文字列　真偽値　配列　

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,13 +3,33 @@ import styles from 'src/styles/Home.module.css';
 import {Footer} from "src/components/Footer"
 import { Main } from "src/components/Main";
 import { Header } from "src/components/Header";
-import { useEffect, useState } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 export default function Home() {
-  const [foo,setFoo] =useState (1);
-  const handleClick = (e) => {
-     //setFoo(foo + 1)
-     setFoo((foo) => foo + 1);
-  }
+
+  const [count,setCount] = useState (1);
+  const [text,setText] = useState ("");
+  const [isShow,setIsShow] = useState (false);
+  const [array,setArray] = useState ([]);
+
+  const handleClick = useCallback(() => {
+     setCount((precount) => precount + 1);
+  },[count]);
+
+  const handleDisplay = useCallback(() => {
+     setIsShow((isShow) => !isShow);
+  },[]);
+
+  const handleAdd= useCallback(() => {
+    setArray((prevArray) => {
+      if (prevArray.some(item => item === text)){
+        alert("同じ要素がすでに存在します。")
+        return　prevArray;
+      }
+     　const newArray = [...prevArray,text]
+     return newArray;
+    });
+  },[text]);
+
   useEffect(() => {
     document.body.style.backgroundColor ="lightgreen";
     return () => {
@@ -23,12 +43,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header />
-      <h1>{foo}</h1>
-      <button 
-      onClick={handleClick
-      }
-      >
-        Count</button>
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleClick}>Count</button>
+      <button onClick={handleDisplay}
+      >{isShow ? "非表示" :"表示" }</button>
+
+      <input type='text' value={text} onChange={ e => {
+        setText(e.target.value);
+      }} />
+      <button onClick={handleAdd}>Add</button>
+　　　<ul>
+        {
+          array.map((item) => {
+            return (
+              <li key={item}>{item}</li>
+            )
+          })}
+      </ul>
       <Main page="index"/>
 
       <Footer/>
